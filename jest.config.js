0522ccdcf26400931978cdaa18fbda705972b3e7
Dimitar Yosifov <dayossifov@gmail.com>
2025-11-26 18:26:00 +0100

    Unit tests with jest -authentication

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
